1fcfa8229834db8b288c9b16b574ff07cd1e00a8
EvilChookie <869063+EvilChookie@users.noreply.github.com>
2024-07-07 16:43:34 +1000

    Update Navigation

--- src/app/_components/layout/sidebar.tsx
+++ src/app/_components/layout/sidebar.tsx
@@ -1,33 +1,72 @@
-import { MagnifyingGlassIcon, HomeIcon } from "@heroicons/react/20/solid";
-import { Sidebar, SidebarHeader, SidebarSection, SidebarItem, SidebarLabel, SidebarBody, SidebarSpacer } from "../catalyst/sidebar";
+import {
+  MagnifyingGlassIcon,
+  HomeIcon,
+  BookOpenIcon,
+  TableCellsIcon,
+  CogIcon,
+  InboxStackIcon,
+  ArrowsRightLeftIcon,
+} from "@heroicons/react/20/solid";
+import {
+  Sidebar,
+  SidebarHeader,
+  SidebarSection,
+  SidebarItem,
+  SidebarLabel,
+  SidebarBody,
+  SidebarSpacer,
+  SidebarFooter,
+} from "../catalyst/sidebar";
 import GithubIcon from "../icons/github";
 
 export default function SidebarContents(): React.ReactNode {
-    return (
-        <Sidebar>
-            <SidebarHeader>
-                <SidebarSection className="max-lg:hidden">
-                    <SidebarItem href="/search">
-                        <MagnifyingGlassIcon />
-                        <SidebarLabel>Search</SidebarLabel>
-                    </SidebarItem>
-                </SidebarSection>
-            </SidebarHeader>
-            <SidebarBody>
-                <SidebarSection>
-                    <SidebarItem href="/">
-                        <HomeIcon />
-                        <SidebarLabel>Home</SidebarLabel>
-                    </SidebarItem>
-                </SidebarSection>
-                {/* <SidebarSpacer />
-                <SidebarSection>
-                    <SidebarItem href="https://github.com/EvilChookie/ShowMyCards">
-                        <GithubIcon />
-                        <SidebarLabel>GitHub</SidebarLabel>
-                    </SidebarItem>
-                </SidebarSection> */}
-            </SidebarBody>
-        </Sidebar>
-    );
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarSection className="max-lg:hidden">
+          <SidebarItem href="/search">
+            <MagnifyingGlassIcon />
+            <SidebarLabel>Search</SidebarLabel>
+          </SidebarItem>
+        </SidebarSection>
+      </SidebarHeader>
+      <SidebarBody>
+        <SidebarSection>
+          <SidebarItem href="/">
+            <HomeIcon />
+            <SidebarLabel>Dashboard</SidebarLabel>
+          </SidebarItem>
+          <SidebarItem href="/collection/">
+            <BookOpenIcon />
+            <SidebarLabel>Your Collection</SidebarLabel>
+          </SidebarItem>
+          <SidebarItem href="/decks/">
+            <TableCellsIcon />
+            <SidebarLabel>Your Decks</SidebarLabel>
+          </SidebarItem>
+          <SidebarItem href="/storage/">
+            <InboxStackIcon />
+            <SidebarLabel>Storage Locations</SidebarLabel>
+          </SidebarItem>
+          <SidebarItem href="/rules/">
+            <ArrowsRightLeftIcon />
+            <SidebarLabel>Storage Rules</SidebarLabel>
+          </SidebarItem>
+          <SidebarItem href="/settings/">
+            <CogIcon />
+            <SidebarLabel>Settings</SidebarLabel>
+          </SidebarItem>
+        </SidebarSection>
+        <SidebarSpacer />
+      </SidebarBody>
+      <SidebarFooter>
+        <SidebarSection>
+          <SidebarItem href="https://github.com/EvilChookie/ShowMyCards">
+            <GithubIcon />
+            <SidebarLabel>GitHub</SidebarLabel>
+          </SidebarItem>
+        </SidebarSection>
+      </SidebarFooter>
+    </Sidebar>
+  );
 }
