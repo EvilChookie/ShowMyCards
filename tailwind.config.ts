7f3115eb20411801f6f0ed64e7205aab009173fa
EvilChookie <869063+EvilChookie@users.noreply.github.com>
2024-07-07 16:43:43 +1000

    Remove safelisted classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,14 +12,4 @@ export default {
     },
   },
   plugins: [Typography],
-  safelist: [
-    "bg-blue-50",
-    "bg-red-50",
-    "bg-green-50",
-    "bg-yellow-50",
-    "dark:bg-blue-100",
-    "dark:bg-red-100",
-    "dark:bg-green-100",
-    "dark:bg-yellow-100",
-  ],
 } satisfies Config;
