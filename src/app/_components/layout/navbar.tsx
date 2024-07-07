46ad29f7bef1bd9151ccd6361305efd81f9e655d
EvilChookie <869063+EvilChookie@users.noreply.github.com>
2024-07-07 16:43:28 +1000

    Adjust Mobile Navbar

--- src/app/_components/layout/navbar.tsx
+++ src/app/_components/layout/navbar.tsx
@@ -1,18 +1,28 @@
 import { MagnifyingGlassIcon, HomeIcon } from "@heroicons/react/20/solid";
-import { Navbar, NavbarSpacer, NavbarSection, NavbarItem } from "../catalyst/navbar";
+import {
+  Navbar,
+  NavbarSpacer,
+  NavbarSection,
+  NavbarItem,
+} from "../catalyst/navbar";
 
+/**
+ * The navigation is shown in small screen scenarios.
+ * It's not the full navigation.
+ * For the full navigation, see ./src/app/_components/layout/sidebar.tsx
+ */
 export default function NavbarContents(): React.ReactNode {
-    return (
-        <Navbar>
-            <NavbarSpacer />
-            <NavbarSection>
-                <NavbarItem href="/search" aria-label="Search">
-                    <MagnifyingGlassIcon />
-                </NavbarItem>
-                <NavbarItem href="/" aria-label="Inbox">
-                    <HomeIcon />
-                </NavbarItem>
-            </NavbarSection>
-        </Navbar>
-    );
+  return (
+    <Navbar>
+      <NavbarSpacer />
+      <NavbarSection>
+        <NavbarItem href="/search" aria-label="Search">
+          <MagnifyingGlassIcon />
+        </NavbarItem>
+        <NavbarItem href="/" aria-label="Home">
+          <HomeIcon />
+        </NavbarItem>
+      </NavbarSection>
+    </Navbar>
+  );
 }
