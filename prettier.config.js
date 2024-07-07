61278e8420f61846d7557e63830686ae02583656
Dave Norton <869063+EvilChookie@users.noreply.github.com>
2024-07-07 22:37:09 +1000

    Update Prettier Config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 120,
+  semi: false,
+  bracketSameLine: true,
+  singleQuote: true,
+  plugins: ["prettier-plugin-tailwindcss"]
 };
 
 export default config;
