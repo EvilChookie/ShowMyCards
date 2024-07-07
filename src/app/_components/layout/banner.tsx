201f7cb78f95425c7062862d66a7948627cc1e0a
EvilChookie <869063+EvilChookie@users.noreply.github.com>
2024-07-07 16:43:14 +1000

    Update Banner component

--- src/app/_components/layout/banner.tsx
+++ src/app/_components/layout/banner.tsx
@@ -1,38 +1,66 @@
-import { CheckCircleIcon, ExclamationTriangleIcon, InformationCircleIcon, QuestionMarkCircleIcon, XCircleIcon } from '@heroicons/react/20/solid'
+import {
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+  InformationCircleIcon,
+  XCircleIcon,
+} from "@heroicons/react/20/solid";
 
 interface BannerProps {
-    type: "info" | "success" | "warning" | "error",
-    heading: string,
-    children: React.ReactNode
+  type: "info" | "success" | "warning" | "error";
+  heading: string;
+  children: React.ReactNode;
 }
 
-type BannerConfiguration = {
-    icon: typeof ExclamationTriangleIcon
-    color: string
-}
+const icons = {
+  info: InformationCircleIcon,
+  success: CheckCircleIcon,
+  warning: ExclamationTriangleIcon,
+  error: XCircleIcon,
+};
 
-const BannerTypes = new Map<string, BannerConfiguration>();
-BannerTypes.set("info", { icon: InformationCircleIcon, color: "blue" });
-BannerTypes.set("success", { icon: CheckCircleIcon, color: "green" });
-BannerTypes.set("warning", { icon: ExclamationTriangleIcon, color: "yellow" });
-BannerTypes.set("error", { icon: XCircleIcon, color: "red" });
+const backgroundColors = {
+  info: "bg-blue-50 dark:bg-blue-100",
+  success: "bg-green-50 dark:bg-green-100",
+  warning: "bg-yellow-50 dark:bg-yellow-100",
+  error: "bg-red-50 dark:bg-red-100",
+};
+
+const iconColors = {
+  info: "fill-blue-400",
+  success: "fill-green-400",
+  warning: "fill-yellow-400",
+  error: "fill-red-400",
+};
+
+const headerColors = {
+  info: "text-blue-800",
+  success: "text-green-800",
+  warning: "text-yellow-800",
+  error: "text-red-800",
+};
+
+const textColors = {
+  info: "text-blue-700",
+  success: "text-green-700",
+  warning: "text-yellow-700",
+  error: "text-red-700",
+};
 
 export default function Banner({ type, heading, children }: BannerProps) {
-    const bannerConfig = BannerTypes.get(type);
-    const Icon = bannerConfig?.icon ?? QuestionMarkCircleIcon
-    return (
-        <div className={`rounded-md bg-${bannerConfig?.color}-50 dark:bg-${bannerConfig?.color}-100 my-4 p-4`}>
-            <div className="flex">
-                <div className="flex-shrink-0">
-                    <Icon className={`h-5 w-5 text-${bannerConfig?.color}-400`} aria-hidden="true" />
-                </div>
-                <div className="ml-3">
-                    <h3 className={`text-sm font-medium text-${bannerConfig?.color}-800`}>{heading}</h3>
-                    <div className={`mt-2 text-sm text-${bannerConfig?.color}-700`}>
-                        {children}
-                    </div>
-                </div>
-            </div>
+  const Icon = icons[type];
+  return (
+    <div className={`my-4 rounded-md p-4 ${backgroundColors[type]}`}>
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <Icon className={`h-5 w-5 ${iconColors[type]}`} aria-hidden="true" />
         </div>
-    )
+        <div className="ml-3">
+          <h3 className={`text-sm font-bold ${headerColors[type]}`}>
+            {heading}
+          </h3>
+          <div className={`mt-2 text-sm ${textColors[type]}`}>{children}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
