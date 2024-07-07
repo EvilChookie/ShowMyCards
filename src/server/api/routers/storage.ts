76c2b1ecebb2478016d2d2e4a7c678624b07d91b
Dave Norton <869063+EvilChookie@users.noreply.github.com>
2024-07-07 22:37:09 +1000

    Storage API

---
+++ src/server/api/routers/storage.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+const storageRouter = createTRPCRouter({
+    list: publicProcedure.query(({ ctx }) => {
+        return ctx.db.storage.findMany({ orderBy: [{ name: 'asc' }] })
+    }),
+
+    getById: publicProcedure.input(z.number()).query(({ ctx, input }) => {
+        return ctx.db.storage.findFirst({ where: { id: input } })
+    }),
+
+    create: publicProcedure.input(z.object({ name: z.string(), type: z.string() })).mutation(async (opts) => {
+        return await opts.ctx.db.storage.create({ data: { name: opts.input.name, type: opts.input.type } })
+    }),
+
+    deleteStorage: publicProcedure.input(z.number()).query(({ ctx, input }) => {
+        return ctx.db.storage.delete({ where: { id: input } })
+    })
+});
+
+export default storageRouter;
