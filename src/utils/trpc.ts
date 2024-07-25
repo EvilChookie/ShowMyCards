bb0efd7c859b630c1b8258712dac4e019472b321
Dave Norton <869063+EvilChookie@users.noreply.github.com>
2024-07-25 22:33:31 +1000

    Auto invalidate queries by on successful mutation

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -7,4 +7,23 @@ export type ReactQueryOptions = inferReactQueryProcedureOptions<AppRouter>
 export type RouterInputs = inferRouterInputs<AppRouter>
 export type RouterOutputs = inferRouterOutputs<AppRouter>
 
-export const trpc = createTRPCReact<AppRouter>()
+export const trpc = createTRPCReact<AppRouter>({
+  overrides: {
+    useMutation: {
+      /**
+       * This function is called whenever a `.useMutation` succeeds
+       **/
+      async onSuccess(opts) {
+        /**
+         * @note that order here matters:
+         * The order here allows route changes in `onSuccess` without
+         * having a flash of content change whilst redirecting.
+         **/
+        // Calls the `onSuccess` defined in the `useQuery()`-options:
+        await opts.originalFn()
+        // Invalidate all queries in the react-query cache:
+        await opts.queryClient.invalidateQueries()
+      },
+    },
+  },
+})
